5f7d0b770b514d2b08776afd45d5bfa7c62af12d
94417 <944176460@qq.com>
2021-03-31 14:00:21 +0800

    UT and init

--- utils/initial.go
+++ utils/initial.go
@@ -1,22 +1,10 @@
 package utils
 
 import (
-	"flag"
-	"github.com/golang/glog"
 	"known01/conf"
-	"math/rand"
-	"time"
 )
 
 func init() {
-	var confFile string
-	flag.StringVar(&confFile, "c", "", "configuration file")
-	flag.Parse()
-	if confFile == "" {
-		glog.Fatal("You must input path of the yml ....")
-	}
-	conf.Init(confFile, &conf.DefaultConfig)
-	rand.Seed(time.Now().UnixNano())
 	InitMySQL(conf.DefaultConfig.DBMysql, true) //建立MySQL连接
 	InitSegDic()                                //初始化分词词表
 }
