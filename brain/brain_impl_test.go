20d092e59516dc7fb2561fc9442c9335035260ae
liqi <sdjyliqi@163.com>
2021-04-06 11:08:31 +0800

    run UT

--- brain/brain_impl_test.go
+++ brain/brain_impl_test.go
@@ -2,17 +2,20 @@ package brain
 
 import (
 	"github.com/go-playground/assert/v2"
+	"github.com/sdjyliqi/known01/testutils"
 	"github.com/sdjyliqi/known01/utils"
 	"testing"
 )
 
 func Test_acFindPhoneNum(t *testing.T) {
+	testutils.Init()
 	c := CreateCenter()
 	v, ok := c.acFindPhoneID(messageTest)
 	t.Log(v, ok)
 }
 
 func Test_amendMessage(t *testing.T) {
+	testutils.Init()
 	c := CreateCenter()
 	testMsg := "爸爸我在外面北京遇到点事，不方便接听电话和短信，给我打10000元到我的工商银行，户主李奇，账号12121212121111"
 	v := c.amendMessage(testMsg)
@@ -24,6 +27,7 @@ func Test_amendMessage(t *testing.T) {
 }
 
 func Test_matchEngineRate(t *testing.T) {
+	testutils.Init()
 	c := CreateCenter()
 	testMsg := "尊敬的用户：您的电子密码器于次日失效，请速登录手机维护网站wap.icbcsap.com进行更新。给你带来不变，敬请谅解,,具体请咨询95588！【工商银行】"
 	rate, engine := c.matchEngineRate(testMsg)
@@ -31,6 +35,7 @@ func Test_matchEngineRate(t *testing.T) {
 }
 
 func Test_acFindIndexWord(t *testing.T) {
+	testutils.Init()
 	c := CreateCenter()
 	testMsg := "尊敬的用户：给我转点钱吧，敬请谅解！【工商银行】"
 	v, ok := c.acFindIndexWord(testMsg)
@@ -39,6 +44,7 @@ func Test_acFindIndexWord(t *testing.T) {
 }
 
 func Test_GetEngineName(t *testing.T) {
+	testutils.Init()
 	c := CreateCenter()
 	//测试包括电话号码的情况
 	testMsg := "尊敬的用户：给我转点钱吧，敬请谅解！95595"
@@ -63,16 +69,17 @@ func Test_GetEngineName(t *testing.T) {
 }
 
 func Test_JudgeMessage(t *testing.T) {
+	testutils.Init()
 	testMsg := "尊敬的用户：您的电子密码器于次日失效，请速登录手机维护网站wap.icbc.com进行更新。给你带来不变，敬请谅解,具体请咨询95588！【工商银行】"
 	c := CreateCenter()
-	suggest, _, score := c.JudgeMessage(testMsg, "95588")
-	t.Log(suggest, score)
+	score, reference := c.JudgeMessage(testMsg, "95588")
+	t.Log(score, reference)
 
 	testMsg = "今天阳光明媚，可以小酌一杯"
-	suggest, _, score = c.JudgeMessage(testMsg, "15210510285")
-	t.Log(suggest, score)
+	score, reference = c.JudgeMessage(testMsg, "15210510285")
+	t.Log(score, reference)
 
 	testMsg = "恭喜！您的手机号码15210510288已经被栏目组随机抽取为场外的幸运用户。将获得由赞助商提供的奖金:价值元的笔记本电脑！详情请登录活动网站:验证码请牢记[中国好声音]"
-	suggest, _, score = c.JudgeMessage(testMsg, "800-12344444")
-	t.Log(suggest, score)
+	score, reference = c.JudgeMessage(testMsg, "800-12344444")
+	t.Log(score, reference)
 }
--- testutils/init.go
+++ testutils/init.go
@@ -3,6 +3,7 @@ package testutils
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
+	"github.com/prometheus/common/log"
 	"github.com/sdjyliqi/known01/conf"
 	"github.com/sdjyliqi/known01/utils"
 )
@@ -11,4 +12,9 @@ var DBEngineTest *xorm.Engine
 
 func init() {
 	DBEngineTest, _ = utils.InitMySQL(conf.DefaultConfig.DBMysql, true)
+	utils.InitSegDic()
+}
+
+func Init() {
+	log.Info("Init mysql client for test")
 }
