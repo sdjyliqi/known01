13db96c0882b3198e056bba66d7f7c3da556f4b7
94417 <944176460@qq.com>
2022-05-13 10:59:14 +0800

    修改数据库表字段名

--- model/dsis_message_templates.go
+++ model/dsis_message_templates.go
@@ -13,7 +13,7 @@ type DsisMessageTemplates struct {
 	CategoryId   int       `json:"category_id" xorm:"not null comment('分类 1银行 2快递 3中奖') INT(4)"`
 	SimHash      int64     `json:"sim_hash" xorm:"not null BIGINT(20)"`
 	Detail       string    `json:"detail" xorm:"not null VARCHAR(1024)"`
-	Enable       int       `json:"enable" xorm:"comment('是否启动 0，未启动，1 启用') TINYINT(3)"`
+	Enable       int       `json:"status" xorm:"comment('是否启动 0，未启动，1 启用') TINYINT(3)"`
 	LastModified time.Time `json:"last_modified" xorm:"not null comment('模板最后更新时间') DATETIME"`
 }
 
